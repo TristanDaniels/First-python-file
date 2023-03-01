0b7b1361ec8bcc0e516c25b110805125f165e3fc
TristanDaniels <tristandaniels07@gmail.com>
2023-03-01 15:06:07 +0200

    update

---
+++ Hello world.py
@@ -0,0 +1,70 @@
+# red_bucket = "Tristan"
+# print(red_bucket)
+
+
+# red_bucket = input("What do you want to put in the bucket?")
+# print(red_bucket)
+
+
+# print(5==4)
+# print(5!=4)
+
+
+# Nick_Age = 10
+# Age_at_kindergarten =5
+
+# if Nick_Age < Age_at_kindergarten:
+#     print("Nick should be at pre-school.")
+# elif Nick_Age == Age_at_kindergarten:
+#     print("Enjoy kindergarten!")    
+# else:
+#     print("Nick should be in another class.")    
+
+
+# def print_tristan():
+#     text = "Tristan is a legend."
+#     print(text)
+
+
+# def school_age_calculator(age,name):
+#     if age < 5:
+#         print("Enjoy your childhood!", name, "is only", age)
+#     elif age == 5:
+#         print("Enjoy kindergarten!", name)
+#     else:
+#         print("they grow up so fast!")
+
+# school_age_calculator(10, "Nick")
+
+
+# def add_ten_to_age(age):
+#     new_age = age +10
+#     return new_age
+
+# how_old_will_I_be = add_ten_to_age(3)
+# print(how_old_will_I_be)
+
+
+# while loop
+# x=0
+# while (x<5):
+#     print(x)
+#     x=x+1
+
+
+#for loop
+
+# for x in range(5,10):
+#     print(x)
+
+
+# days=["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
+
+# for d in days:
+#     # if(d=="Thu"):break
+#     if(d=="Thu"):continue
+#     print(d)
+
+
+# import math
+# print("pi is", math.pi)
